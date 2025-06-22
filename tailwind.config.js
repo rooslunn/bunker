4d124d14a7e375c740d8273214041b0ec4e48b50
Ruslan Kladko <rkladko@gmail.com>
2025-06-22 16:40:25 +0200

    fix: tag text size

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,6 @@ export default {
     content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
     theme: {
         extend: {
-            colors: {
-                black: "#060606",
-            },
-            fontFamily: {
-                "hanken-grotesk": ["Hanken Grotesk", "sans-serif"],
-            },
-            fontSize: {
-                "2xs": ".625rem", // = 10px/16px
-            },
         },
     },
     plugins: [],
