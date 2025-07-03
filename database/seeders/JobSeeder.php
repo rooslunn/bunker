1d95867ec966cd66f96d1fe007b8462cde742754
Ruslan Kladko <rkladko@gmail.com>
2025-07-03 12:43:47 +0200

    seed db before testing

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -2,6 +2,24 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 
-class JobSeeder extends Seeder {}
+class JobSeeder extends Seeder {
+
+    public function run(): void
+
+    {
+        $tags = Tag::factory(3)->create();
+
+        Job::factory(20)->hasAttached($tags)->create(new Sequence([
+            'featured' => false,
+            'schedule' => 'Full Time',
+        ], [
+            'featured' => true,
+            'schedule' => 'Part Time',
+        ]));
+    }
+}
--- tests/Feature/BunkerOnlineTest.php
+++ tests/Feature/BunkerOnlineTest.php
@@ -1,6 +1,12 @@
 <?php
 
+use Database\Seeders\JobSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
 it('is online', function () {
+    $this->seed();
     $response = $this->get('/');
     $response->assertStatus(200);
 });
